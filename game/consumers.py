32a68f7823582a49a0e08edb07be84c472098eb5
Dylan Suen <suen333robot@gmail.com>
2025-05-04 03:36:48 +0800

    refactor: update .gitignore, remove unused TensorFlow code, and replace AI network with random agent

--- game/consumers.py
+++ game/consumers.py
@@ -1,56 +1,55 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from . import big2Game
 from . import enumerateOptions
-from . import PPONetwork
-import tensorflow.compat.v1 as tf
-import joblib
-import os
-from django.conf import settings
+import random
 
-tf.disable_v2_behavior()
-sess = tf.Session()
-trainedAINetwork = PPONetwork.PPONetwork(sess, 412, 1695, "trainedNetwork")
-tf.global_variables_initializer().run(session=sess)
-params = joblib.load(os.path.join(settings.BASE_DIR, "modelParameters136500"))
-trainedAINetwork.loadParams(params)
+class RandomAgent:
+    def step(self, obs, availAcs):
+        # availAcs: list of available actions, 1 for available, 0 or -inf for unavailable
+        # 這裡假設 availAcs 是一個 list 或 numpy array
+        available_indices = [i for i, v in enumerate(availAcs[0]) if v == 0]
+        if not available_indices:
+            return [0], None, None  # fallback
+        action = random.choice(available_indices)
+        return [action], None, None
 
-print("LOADED NETWORK!!!")
+random_agent = RandomAgent()
 
-class GameConsumer(WebsocketConsumer):
-    def connect(self):
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.game = big2Game.big2Game()
         #send initial state - write a function big2Game.infoForDrawing
-        self.accept()
-        self.sendCurrentGameState()
+        await self.accept()
+        await self.sendCurrentGameState()
         
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         pass
         
-    def sendCurrentGameState(self):
-        self.send(text_data=json.dumps(self.game.getInfoForDrawing()))
+    async def sendCurrentGameState(self):
+        await self.send(text_data=json.dumps(self.game.getInfoForDrawing()))
         
-    def receive(self, text_data):
+    async def receive(self, text_data):
         #when we receive something from client side.
         data = json.loads(text_data)
         #import pdb; pdb.set_trace()
         if data['type'] == "AIGo":
             pGo, state, availAcs = self.game.getCurrentState()
-            action, v, nlp = trainedAINetwork.step(state, availAcs)
+            action, v, nlp = random_agent.step(state, availAcs)
             reward, done, info = self.game.step(action[0])
-            self.sendCurrentGameState()
+            await self.sendCurrentGameState()
         elif data['type'] == "reset":
             self.game.reset()
-            self.sendCurrentGameState()
+            await self.sendCurrentGameState()
         elif data['type'] == "pass":
             if self.game.playersGo != 1:
-                self.send(text_data=json.dumps({
+                await self.send(text_data=json.dumps({
                     'type' : "error",
                     'error' : "Wasn't your go ya cheat!"
                 }))
             else:
                 reward, done, info = self.game.step(enumerateOptions.passInd)
-                self.sendCurrentGameState()
+                await self.sendCurrentGameState()
         elif data['type'] == "submitPlayerHand":
             indsPlayed = data['hand']
             if len(indsPlayed)==1:
@@ -71,11 +70,10 @@ class GameConsumer(WebsocketConsumer):
             index = enumerateOptions.getIndex(opt, nC)
             availAcs = self.game.returnAvailableActions()
             if availAcs[index] == 0:
-                self.send(text_data=json.dumps({
+                await self.send(text_data=json.dumps({
                     'type' : "error",
                     'error' : "Server says no. Stop trying to cheat!"
                 }))
             else:
                 reward, done, info = self.game.step(index)
-                self.sendCurrentGameState()
-            
+                await self.sendCurrentGameState()
